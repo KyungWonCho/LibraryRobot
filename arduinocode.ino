e89b81159740f851bf1a4d8fc2b39d4fb3d7b405
Cho Kyung-Won <cscandkswon@hanmail.net>
2019-06-07 00:43:19 +0900

    Update arduinocode.ino

--- arduinocode.ino
+++ arduinocode.ino
@@ -1,9 +1,9 @@
-const int DIR1=22;
-const int ENA1=23;
-const int DIR2=24;
-const int ENA2=25;
-const int DIR3=26;
-const int ENA3=27;
+const int ENA1=22;
+const int DIR1=23;
+const int ENA2=24;
+const int DIR2=25;
+const int ENA3=26;
+const int DIR3=27;
 const int CLK1=5;
 const int CLK2=6;
 const int CLK3=7;
@@ -26,65 +26,100 @@ void setup(){
   digitalWrite(ENA3, HIGH);
 }
 
-void makemove(int st){
-  if(st>0) digitalWrite(DIR1, HIGH);
-  else digitalWrite(DIR1, LOW), st=-st;
-  while(st){
+void makemove(int a){
+  int x=(a-pos)*1000;
+  pos=a;
+  if(x>0) digitalWrite(DIR1, LOW);
+  else digitalWrite(DIR1, HIGH), x=-x;
+  while(x){
     digitalWrite(CLK1, HIGH);
     delay(2);
     digitalWrite(CLK1, LOW);
     delay(2);
-    st--;
+    x--;
   }
 }
 
 void push(){
-  digitalWrite(DIR2, HIGH);
-  st=500;       // test하고 결정
+  digitalWrite(DIR3, LOW);
+  int st=3350;
   while(st){
-    digitalWrite(CLK2, HIGH);
+    digitalWrite(CLK3, HIGH);
     delay(2);
-    digitalWrite(CLK2, LOW);
+    digitalWrite(CLK3, LOW);
     delay(2);
     st--;
   }
 }
 
 void pop(){
-  digitalWrite(DIR2, LOW);
-  st=500;       // test하고 결정
+  digitalWrite(DIR3, HIGH);
+  int st=3350;
   while(st){
-    digitalWrite(CLK2, HIGH);
+    digitalWrite(CLK3, HIGH);
     delay(2);
-    digitalWrite(CLK2, LOW);
+    digitalWrite(CLK3, LOW);
     delay(2);
     st--;
   }
 }
+
+void opening(){
+  digitalWrite(DIR2, LOW);
+  for(int i=0; i<7000; i++){
+    digitalWrite(CLK2, HIGH);
+    delay(1);
+    digitalWrite(CLK2, LOW);
+    delay(1);
+  }
+}
+
+void closing(int style){
+  digitalWrite(DIR2, HIGH);
+  int st=0;
+  switch(style){
+  case 4:
+    st=5200;
+    break;
+  case 5:
+    st=4300;
+    break;
+  case 6:
+    st=3000;
+    break;
+  case 7:
+    st=1900;
+    break;
+  }
+  digitalWrite(DIR2, HIGH);
+  for(int i=0; i<st; i++){
+    digitalWrite(CLK2, HIGH);
+    delay(1);
+    digitalWrite(CLK2, LOW);
+    delay(1);
+  }
+}
+
 void loop(){
   if(Serial.available()){
     char com=Serial.read();
     if(com=='m'){
-      int st=0;
       char a;
-      while(true){
-        a=Serial.read();
-        if(a=='a') break;
-        st=(st*10)+a-'0';
-      }
-      makemove(st-pos);
-      pos=st;
+      a=Serial.read();
+      makemove(a);
     }
-    if(com=='o'){
-      int st=0;
+    if(com=='a'){
       char a;
-      while(true){
-        a=Serial.read();
-        if(a=='a') break;
-        st=(st*10)+a-'0';
-      }
-      
+      a=Serial.read();
+      a=a-'0';
+      opening();
+      delay(100);
+      push();
+      delay(100);
+      closing(a);
+      delay(100);
+      pop();
+      delay(100);
     }
   }
 }
-
