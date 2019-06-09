266df5b7dfa39f0bc4f0d24edab42a49d367fa66
Cho Kyung-Won <cscandkswon@hanmail.net>
2019-06-10 03:02:17 +0900

    Update raspicode.py

--- raspicode.py
+++ raspicode.py
@@ -5,13 +5,14 @@ from picamera import PiCamera
 import serial
 
 HOST, PORT, BUFSIZE = 'localhost', 5000, 1024
-
+'''
 cam=PiCamera()
 ser=serial.Serial('/dev/ttyACM0', 9600)
 sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
-for i in range(4):
+time.sleep(10)
+for i in range(3):
     ser.write(b'm')
     ser.write(str(i).encode('UTF-8'))
     time.sleep(5)
@@ -44,7 +45,7 @@ bcom=sock.recv(BUFSIZE)
 com+=bcom.decode('UTF-8')
 with open('books.txt','w') as inf:
     inf.write(com)
-
+'''
 class book:
     first=''
     second=''
@@ -58,43 +59,52 @@ class book:
         self.width=width
 
 def comp(a, b):
-    if a.first==a.second:
-        if b.first==b.second:
-            return c.first<c.second
-        return b.first<b.second
-    return a.first<a.second
+    if a.first==b.first:
+        if a.second==b.second:
+            return a.third<b.third
+        return a.second<b.second
+    return a.first<b.first
 
 database=open('data.txt', 'r')
 
 datanum=int(database.readline())
 datamap=dict()
 
+print('read database')
+
 for i in range(datanum):
     data=database.readline()
+    print(data[0:-1])
     datal=data.split()
-    print(len(datal))
     if len(datal)==4:
         bookdata=book(datal[1], datal[2], '', int(datal[3]))
     else:
         bookdata=book(datal[1], datal[2], datal[3], int(datal[4]))
     datamap[datal[0]]=bookdata
 
+print('read books')
+
 bookinfo=open('books.txt','r')
 num=int(bookinfo.readline())
+print(num)
 books=[]
 
 for i in range(num):
-    books.append(datamap[(bookinfo.readline())[0:-1]])
+    x=(bookinfo.readline())[0:-1]
+    books.append(datamap[x])
+    print(x, books[i].first, books[i].second, books[i].third)
+    books[i].idx=i
 
 sorted_books=books[:]
 
 for i in range(num):
     for j in range(0, num-i-1):
-        if comp(sorted_books[j], sorted_books[j+1]):
+        if not comp(sorted_books[j], sorted_books[j+1]):
             sorted_books[j], sorted_books[j+1] = sorted_books[j+1], sorted_books[j]
 
 for i in range(num):
     books[sorted_books[i].idx].idx=i
+    print(sorted_books[i].first, sorted_books[i].second, sorted_books[i].third)
 
 x=0
 
@@ -104,42 +114,45 @@ for i in range(num):
     for j in range(i+1, num):
         if (books[j].idx)==i:
             print(j, '->', i)
+            '''
             ser.write(b'm')
             ser.write(str(j).encode('UTF-8'))
             time.sleep((j-x)*4.5)
             ser.write(b'a')
-            ser.write(str(books[j].width).encdoe('UTF-8'))
-            time.sleep(3.5+books[j].width*0.5)
+            ser.write(str(books[j].width).encode('UTF-8'))
+            time.sleep(6+books[j].width*0.5)
             ser.write(b'm')
-            ser.write(b'4')
-            time.sleep((4-j)*4.5)
+            ser.write(b'3')
+            time.sleep((3-j)*4.5)
             ser.write(b'b')
-            time.sleep(4)
+            time.sleep(6)
             ser.write(b'm')
             ser.write(str(i).encode('UTF-8'))
-            time.sleep((4-i)*4.5)
+            time.sleep((3-i)*4.5)
             ser.write(b'a')
-            ser.write(str(books[i].width).encdoe('UTF-8'))
-            time.sleep(3.5+books[i].width*0.5)
+            ser.write(str(books[i].width).encode('UTF-8'))
+            time.sleep(6+books[i].width*0.5)
             ser.write(b'm')
             ser.write(str(j).encode('UTF-8'))
             time.sleep((j-i)*4.5)
             ser.write(b'b')
-            time.sleep(4)
+            time.sleep(6)
             ser.write(b'm')
-            ser.write(b'4')
-            time.sleep((4-j)*4.5)
+            ser.write(b'3')
+            time.sleep((3-j)*4.5)
             ser.write(b'a')
-            ser.write(str(books[j].width).encdoe('UTF-8'))
-            time.sleep(3.5+books[j].width*0.5)
+            ser.write(str(books[j].width).encode('UTF-8'))
+            time.sleep(6+books[j].width*0.5)
             ser.write(b'm')
             ser.write(str(i).encode('UTF-8'))
-            time.sleep((4-i)*4.5)
+            time.sleep((3-i)*4.5)
             ser.write(b'b')
-            time.sleep(4)
+            time.sleep(6)
+            '''
             temp=books[j]
             del(books[j])
             books.insert(i, temp)
+            break
     x=i
 
-sock.close()
+#sock.close()
